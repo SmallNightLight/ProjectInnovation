dcb71f6253696ef780f94ae83a2a2276265cb212
SmallNightLight <matthiaskruis@gmail.com>
2024-02-12 12:03:57 +0100

    Unity files

---
+++ Project Innovation/Assets/Scripts/Scriptables/Core/IDataPoint.cs
@@ -0,0 +1,7 @@
+namespace ScriptableArchitecture.Core
+{
+    /// <summary>
+    /// Interface for the serialized datastructures, does not add functionality, but it used for editor drawers
+    /// </summary>
+    public interface IDataPoint { }
+}
