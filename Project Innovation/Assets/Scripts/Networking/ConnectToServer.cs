8fa1d9073ba37c1eedb4103199759dda42f946f4
uwutoria <viktoria0809.0809@gmail.com>
2024-03-04 16:43:26 +0100

    Background pattern test
    :)

--- Project Innovation/Assets/Scripts/Networking/ConnectToServer.cs
+++ Project Innovation/Assets/Scripts/Networking/ConnectToServer.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using System.Collections;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     [SerializeField] private string _nextSceneName;
+    [SerializeField] private VideoPlayer video;
+    [SerializeField] GameObject text; 
+
+    bool _hasJoined, videoEnded;
 
     private void Start()
     {
+        video.loopPointReached += End;
+
         Debug.Log("Trying to connect...");
         PhotonNetwork.ConnectUsingSettings();
+
+        StartCoroutine(WaitForVideo());
     }
     public override void OnConnectedToMaster()
     {
@@ -20,6 +30,25 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         Debug.Log("Joined lobby");
+        _hasJoined = true;
+    }
+
+    public void End(VideoPlayer p) 
+    { 
+        videoEnded = true;
+        p.gameObject.SetActive(false);
+        text.SetActive(true);
+    }
+
+    public IEnumerator WaitForVideo()
+    {
+        while (true)
+        {
+            if (_hasJoined && videoEnded) break;
+
+            yield return null;
+        }
+
         SceneManager.LoadScene(_nextSceneName);
     }
 }
