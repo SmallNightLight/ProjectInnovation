1f8c4ed1e046ca5bd71ec30032c17497a0f0af2d
SmallNightLight <matthiaskruis@gmail.com>
2024-03-07 19:06:57 +0100

    animation fix

--- Project Innovation/Assets/Scripts/Character/CharacterAnimation.cs
+++ Project Innovation/Assets/Scripts/Character/CharacterAnimation.cs
@@ -1,16 +1,10 @@
-using ScriptableArchitecture.Data;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterBase))]
 public class CharacterAnimation : MonoBehaviour
 {
-    [Header("Data")]
-
-    [Header("Settings")]
-    [SerializeField] private FloatReference _characterSpeed;
-
     [Header("Components")]
-    [SerializeField] private Animator _animator;
+    private Animator _animator;
     private CharacterBase _characterBase;
     private CharacterWeapon _characterWeapon;
     private Vector2 _oldDirection;
@@ -34,25 +28,44 @@ public class CharacterAnimation : MonoBehaviour
         bool isWalking = movementDirection.magnitude > 0.01f;
         bool hasGun = _characterWeapon?.IsCarryingWeapon() ?? false;
 
-        float angleInRadians = (Vector2.SignedAngle(facingDirection, movementDirection) + 90) * Mathf.Deg2Rad;
-        Vector2 animationDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians)) * _characterSpeed.Value;
+        Animator animator = Animator;
 
         if (isWalking)
         {
-            _animator.SetFloat("MovementHorizontal", -animationDirection.y, 0.1f, Time.deltaTime);
-            _animator.SetFloat("MovementVertical", animationDirection.x, 0.1f, Time.deltaTime);
+            facingDirection = new Vector2(facingDirection.y, -facingDirection.x);
+
+            if (facingDirection == Vector2.zero)
+                facingDirection = _oldDirection;
+
+            float angleInRadians = (Vector2.SignedAngle(facingDirection, movementDirection) + 90) * Mathf.Deg2Rad;
+            Vector2 animationDirection = new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians));
+
+            animator.SetFloat("MovementHorizontal", animationDirection.x, 0.1f, Time.deltaTime);
+            animator.SetFloat("MovementVertical", animationDirection.y, 0.1f, Time.deltaTime);
+
+            _oldDirection = facingDirection;
         }
         else
         {
-            _animator.SetFloat("MovementHorizontal", 0, 0.1f, Time.deltaTime);
-            _animator.SetFloat("MovementVertical", 0, 0.1f, Time.deltaTime);
+            animator.SetFloat("MovementHorizontal", 0, 0.1f, Time.deltaTime);
+            animator.SetFloat("MovementVertical", 0, 0.1f, Time.deltaTime);
         }
 
         float turningAngle = Mathf.Clamp(Vector2.SignedAngle(facingDirection, _oldDirection), -1, 1);
-        _animator.SetFloat("TurningAngle", turningAngle, 0.2f, Time.deltaTime);
+        animator.SetFloat("TurningAngle", turningAngle, 0.2f, Time.deltaTime);
 
-        _oldDirection = facingDirection;
-        _animator.SetBool("IsWalking", isWalking);
-        _animator.SetBool("HasGun", hasGun);
+        animator.SetBool("IsWalking", isWalking);
+        animator.SetBool("HasGun", hasGun);
+    }
+
+    public Animator Animator 
+    { 
+        get 
+        { 
+            if (_animator == null)
+                _animator = GetComponentInChildren<Animator>();
+                
+            return _animator; 
+        } 
     }
 }
